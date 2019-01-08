b36733fabc37ccbb4fca2ff0593af73c3abda5d6
Kael Spencer <kaelspencer@gmail.com>
2019-01-07 16:42:36 -0800

    wip

--- node_helper.js
+++ node_helper.js
@@ -6,6 +6,7 @@ module.exports = NodeHelper.create({
     start: function() {
         console.log("Starting node_helper for module [" + this.name + "]");
         this.paprikaApi = null;
+        this.outstandingRequest = false;
     },
 
     socketNotificationReceived: function(notification, payload){
@@ -19,26 +20,63 @@ module.exports = NodeHelper.create({
                     this.paprikaApi = new PaprikaApi.PaprikaApi(payload.email, payload.password);
                 }
 
+                if (this.outstandingRequest == true) {
+                    console.log('MMM-PaprikaMenu: Outstanding request, not trying again.');
+                    return;
+                }
+
                 var self = this;
 
-                this.paprikaApi.meals().then((meals) => {
-                    filtered = self.filterMealsByDate(meals, payload.weekStartsOnSunday);
+                this.getMeals(payload.weekStartsOnSunday)
+                .then((meals) => {
                     resp = {
                         instanceId: payload.instanceId,
-                        meals: filtered
+                        meals: meals
                     };
                     self.sendSocketNotification("PAPRIKA_MENU_DATA", resp);
+                })
+                .catch((error) => {
+                    console.log('MMM-PaprikaMenu: Caught an exception during sync.');
+                    console.log(error);
+                })
+                .then(() => {
+                    self.outstandingRequest = false;
                 });
             }
         }
     },
 
+    getMeals: function(weekStartsOnSunday) {
+        var self = this;
+
+        // Get the full list of meals from Paprika.
+        return this.paprikaApi.meals()
+        .then((meals) => {
+            // Filter the whole list down to just our current weak timespan.
+            return self.filterMealsByDate(meals, weekStartsOnSunday);
+        })
+        .then((filteredMeals) => {
+            // We have a filtered set of meals. Now, query Paprika for recipe information as appropriate.
+            // Some meal entries may not have an associated recipe. For example, you entered "Leftovers" on one day.
+            var recipePromises = [];
+
+            filteredMeals.forEach(function(meal) {
+                if (meal.recipe_uid == null) {
+                    // This is a hand-edited meal. No recipe to fetch.
+                } else {
+                    // Fetch the recipe from Paprika.
+                }
+            });
+            return filteredMeals;
+        });
+    },
+
     filterMealsByDate: function(raw, weekStartsOnSunday) {
         // raw is array of:
-        // { uid: '8d95f33c-2fb9-4c1a-814f-b64fe9e50bca',
+        // { uid: 'd95f33c-2fb9-4c1a-814f-b64fe9e50bca',
         //   type_uid: null,
         //   order_flag: 45,
-        //   recipe_uid: null,
+        //   recipe_uid: 'e5293519-8492-4643-b741-3d46f0a9714b',
         //   date: '2018-12-16 00:00:00',
         //   type: 2,
         //   name: 'Breakfast quesadilla' }
@@ -47,7 +85,7 @@ module.exports = NodeHelper.create({
         var startOfWeek = this.getFirstDayOfWeek(weekStartsOnSunday);
         var nextWeek = startOfWeek.clone().add(7, 'days');
 
-        console.log('Week starts: ' + startOfWeek.format('YYYY-MM-DD') + ', next week starts: ' + nextWeek.format('YYYY-MM-DD'));
+        console.log('MMM-PaprikaMenu: Week starts: ' + startOfWeek.format('YYYY-MM-DD') + ', next week starts: ' + nextWeek.format('YYYY-MM-DD'));
 
         raw.forEach(function(item) {
             var itemDate = moment(item.date);
@@ -79,4 +117,19 @@ module.exports = NodeHelper.create({
 
         return firstDayOfWeek.startOf('day');
     },
+
+    constructMealObject: function(mealFromApi) {
+        return {
+            // Fields from the raw Paprika Meal API
+            meal_uid: mealFromApi.uid,
+            type_uid: mealFromApi.type_uid,
+            order_flag: mealFromApi.order_flag,
+            recipe_uid: mealFromApi.recipe_uid,
+            date: mealFromApi.date,
+            type: mealFromApi.type,
+            name: mealFromApi.name,
+            // Fields we care about from the Recipe API.
+            image_url: null,
+        }
+    };
 });
